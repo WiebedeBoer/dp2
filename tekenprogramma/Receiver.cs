755adc152984046a892574a4014c7f11a61d021c
WiebedeBoer <wiebe81@gmail.com>
2021-01-17 12:25:32 +0100

    undo redo

---
+++ tekenprogramma/Receiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tekenprogramma
+{
+    public class Receiver
+    {
+
+        private List<ICommand> action = new List<ICommand>();
+
+        public void takeOrder(ICommand order)
+        {
+            action.Add(order);
+        }
+
+        public void placeOrders()
+        {
+
+            foreach (ICommand order in action)
+            {
+                order.Execute();
+            }
+            action.Clear();
+        }
+    }
+
+    //private string action;
+
+    //public Receiver(string action)
+    //{
+    //    this.action = action;
+    //}
+
+    //public void Actions(string action)
+    //{
+    //this.action = action;
+    //if (action == "create rectangle")
+    //{
+    //MakeRectangle.Execute();
+    //}
+    //else if(action =="create elipse"){
+    //MakeEllipse.Execute();
+    //}            
+    //}
+
+
+}
